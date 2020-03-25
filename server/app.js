7bda9845db4e101ed01d6515670d86f444b1c502
Austin Monson <austinmonson1884@gmail.com>
2020-03-25 21:58:09 +0000

    Login System v1 Finished v1 of the login system, need to add more things but will switch my focus to the devices for now. Also laid some of the groundwork for device registration

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
+const path = require('path');
 const WebSocket = require('ws')
 const mysql = require('mysql')
 const url = require('url')
@@ -20,64 +21,172 @@ db.connect((err) => {
 /*
     HTTP Webserver
 */
-function validate_token(sessionID) {
-    if (sessionID > 0)
-        return true
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/html'));
+//Express middleware requiring token for page.
+function require_token(req, res, next) {
+    if(req.params['token'])
+        token = req.params['token']
     else
-        return true
-}
-function require_valid_session(req, res, next) {
-    if (!validate_token(req.cookies['token']))
+        token = req.cookies['token']
+    if(token === undefined){
         res.redirect('/login')
-    else
-        return next();
+        return
+    }
+    var token_sqs = "SELECT * FROM auth_token WHERE CURRENT_TIMESTAMP < date + INTERVAL 24 HOUR AND active = 1 AND token = ?"
+    var result = db.query(token_sqs, token, (err, result) =>{
+        if (result.length <= 0){
+            res.cookie('token', "", {
+                maxAge: -1// Remove Cookie
+            })
+            res.redirect('/login')
+        }
+        else
+            next();
+    })
 }
-app.get('/', require_valid_session, (req, res) => {
-    res.cookie('session', '1')
-    res.send("Done.")
+app.get('/', require_token, (req, res) => {
+    var username_sqs = "SELECT * FROM auth_user A LEFT JOIN auth_token B ON B.ref_id = A.user_id WHERE token = ?"
+    db.query(username_sqs, [req.cookies['token']], (err, username_result) => {
+        res.render('index', 
+            {
+                username: username_result[0]['username']
+            })
+    })
 })
 app.get('/login', (req, res) => {
-    res.sendFile('./html/login.html', { root: __dirname })
+    query = req.query
+    if(req.cookies['token']){
+        var token_sqs = "SELECT * FROM auth_token WHERE CURRENT_TIMESTAMP < date + INTERVAL 24 HOUR AND active = 1 AND token = ?"
+        var result = db.query(token_sqs, req.cookies['token'], (err, result) =>{
+            if (result.length > 0){
+                res.redirect('/')
+                return;
+            }else{
+                res.cookie('token', "", {
+                    maxAge: -1 // unset cookie
+                })
+                res.redirect('/login');
+                return;
+            }
+        })
+    } else{
+        if(!query['username'] || !query['password']){
+            res.sendFile('./html/login.html', { root: __dirname })
+        }
+        else{
+            var username = query['username']
+            var password = query['password']
+            
+            //Check if username even exists
+            var username_sqs = "SELECT * FROM auth_user WHERE username = ?"
+            db.query(username_sqs, [username], (err, username_result) => {
+                if(username_result.length <= 0){
+                    res.redirect('/login?error=Invalid%20Credentials')
+                    return;
+                }
+                var user_id = username_result[0]['user_id']
+                var secret_sqs = "SELECT * FROM auth_secret WHERE secret_id = ?"
+                db.query(secret_sqs, [username_result[0]['secret_id']], (err, secret_result) => {
+                    var user_password = secret_result[0]['password_hash']
+                    var password_hash = crypto.createHmac('sha512', secret_result[0]['password_salt']).update(password).digest('base64')
+                    if(user_password === password_hash){
+                        var token = uuidv4()
+                        var token_iqs = "INSERT INTO auth_token(token, user_id) VALUES (?,?)"
+                        db.query(token_iqs, [token, user_id], (err, res3) => {
+                            res.cookie('token', token, {
+                                maxAge: 24 * 60 * 60 * 1000 // 1 Day login
+                            })
+                            res.redirect('/')
+                            return
+                        })
+                    }
+                })
+            })
+        }
+    }
+
 })
+app.get('/logout', (req,res) => {
+    if(req.cookies['token']){
+        var disable_token = "UPDATE auth_token SET active=false WHERE token=?"
+        db.query(disable_token, [req.cookies['token']], (err, result) => {
+            res.cookie('token', "", {
+                maxAge: -1 // unset cookie
+            })
+            res.redirect('/')
+            return
+        })
+    }else{
+        res.redirect('/')
+        return
+    }
+});
 app.get('/register', (req, res) => {
     query = req.query
-    if(!query['username'] || !query['password'] || !query['email'])
-        res.sendFile('./html/register.html', { root: __dirname })
-    else{
-        var username = query['username']
-
-        var user_sqs = "SELECT * FROM auth_user WHERE username = ?"
-        db.query(user_sqs, [username], (err, resu) => {
-            if(resu.length > 0){
-                res.redirect('/register?error=Username%20Taken')   
-                return
+    if(req.cookies['token']){
+        var token_sqs = "SELECT * FROM auth_token WHERE CURRENT_TIMESTAMP < date + INTERVAL 24 HOUR AND active = 1 AND token = ?"
+        var result = db.query(token_sqs, req.cookies['token'], (err, result) =>{
+            if (result.length > 0){
+                res.redirect('/')
+                return;
+            }else{
+                res.cookie('token', "", {
+                    maxAge: -1 // unset cookie
+                })
+                res.redirect('/register')
+                return;
             }
-            var password = query['password']
-            var email = query['email']
-    
-            var password_salt = crypto.randomBytes(32).toString('base64')
-            var password_hash = crypto.createHmac('sha512', password_salt).update(password).digest('base64')
-    
-            var pass_iqs = "INSERT INTO auth_secret(password_salt, password_hash) VALUES (?,?)"
-            db.query(pass_iqs, [ password_salt, password_hash], (err, res1) => {
-                var secret_id = res1.insertId;
-                var user_iqs = "INSERT INTO auth_user(username, email_address, secret_id) VALUES(?,?,?)"
-                db.query(user_iqs, [username, password, secret_id], (err, res2) => {
-                    var user_id = res2.insertId
-                    var token = uuidv4()
-                    var token_iqs = "INSERT INTO auth_token(token, user_id) VALUES (?,?)"
-                    db.query(token_iqs, [token, user_id], (err, res3) => {
-                        res.cookie('token', token, {
-                            maxAge: 24 * 60 * 60 * 1000 // 1 Day login
+        })
+    } else{
+        if(!query['username'] || !query['password'] || !query['email'])
+        res.sendFile('./html/register.html', { root: __dirname })
+        else{
+            console.log(query)
+            var username = query['username']
+
+            var user_sqs = "SELECT * FROM auth_user WHERE username = ?"
+            db.query(user_sqs, [username], (err, resu) => {
+                if(resu.length > 0){
+                    res.redirect('/register?error=Username%20Taken')   
+                    return
+                }
+                var password = query['password']
+                var email = query['email']
+        
+                var password_salt = crypto.randomBytes(32).toString('base64')
+                var password_hash = crypto.createHmac('sha512', password_salt).update(password).digest('base64')
+        
+                var pass_iqs = "INSERT INTO auth_secret(password_salt, password_hash) VALUES (?,?)"
+                db.query(pass_iqs, [ password_salt, password_hash], (err, res1) => {
+                    var secret_id = res1.insertId;
+                    var user_iqs = "INSERT INTO auth_user(username, email_address, secret_id) VALUES(?,?,?)"
+                    db.query(user_iqs, [username, email, secret_id], (err, res2) => {
+                        var user_id = res2.insertId
+                        //User is registered, log them in
+                        var token = uuidv4()
+                        var token_iqs = "INSERT INTO auth_token(token, ref_id, token_type) VALUES (?,?, (SELECT token_type_id FROM lut_token_types WHERE token_type_desc = 'user'))"
+                        db.query(token_iqs, [token, user_id], (err, res3) => {
+                            res.cookie('token', token, {
+                                maxAge: 24 * 60 * 60 * 1000 // 1 Day login
+                            })
+                            res.redirect('/login')
+                            return
                         })
-                        res.redirect('/login')
-                        return
                     })
                 })
             })
-        })
+        }
     }
 })
+app.post('/ajax/getAddDeviceForm', (req, res) => {
+    res.sendFile('./html/form/')
+})
+app.post('/ajax/generateDevice', (req, res) => {
+    
+    res.send('Hello?')
+})
+
 app.use('/css', express.static(__dirname + '/html/css'))
 
 app.listen('8000', () => console.log('[Server] Web Server Started'))
